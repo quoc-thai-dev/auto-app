c1c26832893baf441bdeb800cc846dd1d925d750
Nguyễn Dương Quốc Thái <quocthai@192.168.2.130>
2025-10-19 10:53:49 +0700

    Hello

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Auto nextjs
+            Auto nextjs is here
           </li>
         </ol>
 
